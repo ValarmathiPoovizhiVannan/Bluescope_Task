95309f9d9810e4742dca5a1573c1fad4efc7fca3
Valarmathi J P <146519914+ValarmathiPoovizhiVannan@users.noreply.github.com>
2025-12-15 21:15:13 +0530

    Find Salary of Employee

---
+++ src/com/bluescope/Employee.java
@@ -0,0 +1,16 @@
+package com.bluescope;
+
+class Employee {
+	int id;
+	String name;
+	String gender;
+	double salary;
+   Employee(int id, String name, String gender,double salary) {
+	   this.id=id;
+	   this.name=name;
+	   this.gender=gender;
+	   this.salary=salary;
+	   
+ }
+    
+}
